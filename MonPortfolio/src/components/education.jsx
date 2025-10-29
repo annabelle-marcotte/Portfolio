cff4e90b556262af37af1d9184cea1d794b2e049
annnnabelle <aj.marcotte@icloud.com>
2025-10-29 14:32:52 -0400

    education

--- MonPortfolio/src/components/education.jsx
+++ MonPortfolio/src/components/education.jsx
@@ -4,10 +4,22 @@ import "../css/education.css";
 const EducationContent = () => {
 
     return (
+        <div>
         <div className='education-content'>
-            <h1>Mes Projets</h1>
+            <h1>Mon Éducation</h1>
+            <div className="education-grid">
+                <h2>
+                    Techniques de l'informatique
+                </h2>
+                <p>
+                    Diplôme d’études collégiales (DEC) en informatique <br />Collège Lionel-Groulx, en cours
+                </p>
+            </div>
 
         </div>
+                    <div className="about-line"></div>
+        </div>
+        
     );
 };
 
